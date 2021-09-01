dfd71b967c93a1e348d383d3b415f72b12096f62
Mateo Negri <mateonegriuns@gmail.com>
2021-09-01 16:52:21 -0300

    Correcion de errores en mail y direccion de GitHub

--- scr/launcher/Launcher.java
+++ scr/launcher/Launcher.java
@@ -7,7 +7,7 @@ public class Launcher {
 	public static void main(String [] args) {
 		//Schedule a job for the event-dispatching thread:
 	    //creating and showing this application's GUI.
-		Student alumno= new Student(126293,"Negri","Mateo","mateonergiuns@gmail.com","https://github.com/MateChola/tdp-proyecto-1","/images/MC ID.png");
+		Student alumno= new Student(126293,"Negri","Mateo","mateonegriuns@gmail.com","https://github.com/MateChola/tdp-proyecto-1","/images/MC ID.png");
 	    javax.swing.SwingUtilities.invokeLater(new Runnable() {
 	           public void run() {
 	        	   SimplePresentationScreen screen= new SimplePresentationScreen(alumno);
